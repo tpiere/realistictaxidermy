cee9405544c8e1ffdfc603b614a772cd405b07e7
Tom Piere <tpiere@gmail.com>
2015-11-21 16:50:07 -0600

    replaced gallery and update js

---
+++ js/gallery.js
@@ -0,0 +1,11 @@
+void function($){
+ 'use strict';
+
+	$(document).ready(function () {
+	      $("#nanoGallery2").nanoGallery({
+		  kind: 'picasa',
+		  userID: 'realistictaxidermyMn'
+	      });
+  	});
+
+}(jQuery);
